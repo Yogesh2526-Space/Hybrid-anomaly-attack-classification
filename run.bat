ae8c838fc66bca5e514842e3e5efecceb3f7fb95
Yogesh <yogeshyoki03@gmail.com>
2026-06-08 11:41:09 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,8 @@
+cd \
+cd C:\Users\acer\OneDrive\Desktop\Project\Code\Client
+START %windir%\System32\cmd.exe "/K" C:\Users\acer\anaconda3\Scripts\activate.bat C:\Users\acer\anaconda3
+
+cd C:\Users\acer\OneDrive\Desktop\Project\Code\Server
+START %windir%\System32\cmd.exe "/K" C:\Users\acer\anaconda3\Scripts\activate.bat C:\Users\acer\anaconda3
+
+
